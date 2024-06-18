c95e22efbfa4a7b71b21765fa53063bef2c707a6
James Gray <zaargy@gmail.com>
2024-06-18 09:29:42 +0100

    change config path

--- braindump_config.py
+++ braindump_config.py
@@ -1,14 +1,13 @@
 import configparser
 import os
 
-from PyQt5.QtCore import QStandardPaths, QDir
+from PyQt5.QtCore import QDir
 from PyQt5.QtWidgets import QMessageBox
+
 
 class BraindumpConfig:
     def __init__(self):
-        standard_config_path = QStandardPaths.writableLocation(QStandardPaths.AppConfigLocation)
-
-        config_dir = os.path.join(standard_config_path, "braindump")
+        config_dir = os.path.join(os.environ["HOME"], "braindump")
 
         if not QDir(config_dir).exists():
             QDir().mkpath(config_dir)
@@ -20,19 +19,22 @@ class BraindumpConfig:
         self.config = configparser.ConfigParser()
 
         if not os.path.exists(self.config_file):
-            self.config['Email'] = {
-                'sender_email': 'your_email@example.com',
-                'receiver_email': 'receiver_email@example.com',
-                'smtp_server': 'smtp.example.com',
-                'smtp_port': '587',
-                'smtp_tls': 'true',
-                'smtp_ssl': 'false',
-                'username': 'your_username',
-                'password': 'your_password'
+            self.config["Email"] = {
+                "sender_email": "your_email@example.com",
+                "receiver_email": "receiver_email@example.com",
+                "smtp_server": "smtp.example.com",
+                "smtp_port": "587",
+                "smtp_tls": "true",
+                "smtp_ssl": "false",
+                "username": "your_username",
+                "password": "your_password",
             }
-            with open(self.config_file, 'w') as configfile:
+            with open(self.config_file, "w") as configfile:
                 self.config.write(configfile)
-            QMessageBox.information(None, "Configuration Created",
-                                    f"The configuration file has been created at: {self.config_file}")
+            QMessageBox.information(
+                None,
+                "Configuration Created",
+                f"The configuration file has been created at: {self.config_file}",
+            )
 
         self.config.read(self.config_file)
