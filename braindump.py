553bed94261d29eccb430ef711b79d4def0c090b
James Gray <zaargy@gmail.com>
2024-06-20 07:05:53 +0100

    cleanup config

--- braindump.py
+++ braindump.py
@@ -8,9 +8,7 @@ from PyQt5.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
     QWidget,
-    QHBoxLayout,
-    QSpacerItem,
-    QSizePolicy,
+    QHBoxLayout
 )
 from PyQt5.QtCore import Qt, QTimer, QThread
 from PyQt5.QtGui import QFont
